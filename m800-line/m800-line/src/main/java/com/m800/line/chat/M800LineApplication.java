561f8814c3e0aa3a7a11b059382146e2730fb3b1
bruce.su <huanchensu@gamil.com>
2023-02-17 18:30:59 +0800

    setup a Sprint Boot project

---
+++ m800-line/m800-line/src/main/java/com/m800/line/chat/M800LineApplication.java
@@ -0,0 +1,13 @@
+package com.m800.line.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M800LineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M800LineApplication.class, args);
+	}
+
+}
